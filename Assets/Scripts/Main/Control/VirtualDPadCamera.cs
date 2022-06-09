bb5ec6a20aa032c8ecc2b2d4dce091846e5eea75
Jameschungdev <jameschung.imperial@gmail.com>
2022-06-09 23:18:25 +0800

    zoom (not finished)

--- Assets/Scripts/Main/Control/VirtualDPadCamera.cs
+++ Assets/Scripts/Main/Control/VirtualDPadCamera.cs
@@ -12,12 +12,14 @@ public class VirtualDPadCamera : MonoBehaviour
     private Vector2 touchStartCoord, touchEndCoord;
     private string directionString;
     public GameObject cameraTarget;
+    public Camera mainCamera;
     public float scrollingSpeed = 0.005f;
+    public float zoomingSpeed = 0.01f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount == 1)
+        if (Input.touchCount == 1) // Panning Camera
         {
             touchObj = Input.GetTouch(0);
 
@@ -25,7 +27,6 @@ public class VirtualDPadCamera : MonoBehaviour
             {
                 touchStartCoord = touchObj.position;
             }
-            //else if (touchObj.phase == TouchPhase.Moved || touchObj.phase == TouchPhase.Ended)
             else if (touchObj.phase == TouchPhase.Moved || touchObj.phase == TouchPhase.Stationary)
             {
                 touchEndCoord = touchObj.position;
@@ -83,6 +84,47 @@ public class VirtualDPadCamera : MonoBehaviour
 
                 directionText.text = directionString;
             }
+        }
+        else if (Input.touchCount == 2) // Zooming in camera. 
+        {
+            Touch touchObj_1 = Input.GetTouch(0);
+            Touch touchObj_2 = Input.GetTouch(1);
+
+            Vector2 touchStartCoord_1, touchEndCoord_1, touchStartCoord_2, touchEndCoord_2;
+
+            float initialDisplacement = 0.0f;
+
+            if (touchObj_1.phase == TouchPhase.Began || touchObj_2.phase == TouchPhase.Began)
+            {
+                touchStartCoord_1 = touchObj_1.position;
+                touchStartCoord_2 = touchObj_2.position;
+
+                initialDisplacement = Vector2.Distance(touchStartCoord_1, touchStartCoord_2);
+
+                directionString = "Zoom: Began";
+            }
+
+            if ((touchObj_1.phase == TouchPhase.Moved || touchObj_1.phase == TouchPhase.Stationary) &&
+                (touchObj_2.phase == TouchPhase.Moved || touchObj_2.phase == TouchPhase.Stationary))
+            {
+                touchEndCoord_1 = touchObj_1.position;
+                touchEndCoord_2 = touchObj_2.position;
+
+                float currentDisplacement = Vector2.Distance(touchEndCoord_1, touchEndCoord_2);
+
+                if (currentDisplacement > initialDisplacement)
+                {
+                    mainCamera.orthographicSize += zoomingSpeed;
+                    directionString = "Zoom: Zooming in.";
+                }
+                else if (currentDisplacement < initialDisplacement)
+                {
+                    mainCamera.orthographicSize -= zoomingSpeed;
+                    directionString = "Zoom: Zooming Out.";
+                }
+            }
+
+            directionText.text = directionString;
         }
     }
 }
